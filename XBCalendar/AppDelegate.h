17610d29991ce1e9213508261f3d74cbdf2d8a04
1174495446@qq.com <1174495446@qq.com>
2017-09-06 14:31:36 +0800

    Initial Commit

---
+++ XBCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBCalendar
+//
+//  Created by xxb on 2017/9/6.
+//  Copyright © 2017年 com.xb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
